778791b7d25e1a696d3caa983c45553c7bf24d91
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-02-13 12:57:10 +0000

    primo commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#Esercizio:
+#realizzare un server web con flask che offra le seguenti funzionalità.
+#1. homepage deve fornire una breve descrizione delle caratteristiche della città di milano
+#2. al link <nome sito>/immagini deve essere visualizzate le immagini di 4 monumenti di milano (controllare sul sito del prof come si fa a mettere le immagini)
+#3. fare in modo che cliccando sull'immagine si venga mandati ad un brevo testo descrittivo del monumento (sito prof)
+
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def milano():
+    return render_template('milano.html')
+
+@app.route("/immagini")
+def monumenti():
+    return render_template("immagini.html")
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
